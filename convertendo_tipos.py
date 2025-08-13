0c2d2d62972ae2723b6e1a267b986ea2c9f38f68
Ana Sato <anasato.est@gmail.com>
2025-08-13 18:04:52 -0300

    Initial commit

---
+++ convertendo_tipos.py
@@ -0,0 +1,11 @@
+print(int(1.0))
+
+print(int("10"))
+
+print(float(100))
+
+print(str(10.10))
+
+print(100 / 2)
+
+print(100 // 2)
---
+++ variaveis_e_constantes.py
@@ -0,0 +1,9 @@
+nome = 'Guilherme'
+idade = 26
+
+nome = 'Ana'
+print(nome, idade)
+
+STATES = ['SP', 'RJ', 'SC']
+
+print(STATES)
